7d4de3d5bfdeabaff7fd9b1525e5ba376ea66f83
Lindsay Chapin <lnchapin@gmail.com>
2018-02-12 12:11:36 -0700

    migration

--- migrations/20180209114150_questions.js
+++ migrations/20180209114150_questions.js
@@ -3,7 +3,10 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('questions', function (table){
     table.increments('id').primary()
     table.varchar('questionText')
-    table.text('questionText')
+    table.text('correctAnswer')
+    table.text('incorrectAnswer1')
+    table.text('incorrectAnswer2')
+    table.text('incorrectAnswer3')
   })
 };
 
